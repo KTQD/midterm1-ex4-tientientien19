12b2674970e106eab2964c4ce41f2c4611febf52
tientientien19 <133378192+tientientien19@users.noreply.github.com>
2024-03-16 14:44:07 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,35 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Main {
+
     public static void main(String[] args) {
         int[] numbers = {1, 3, 5, 6, 2, 7, 8, 0, 4, 3, 9, 2, 8, 1, 0, 5, 7, 4, 6, 9, 3, 2, 1, 8, 4, 0, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0};
-    
 
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
 
-        Viết mã của bạn vào đây
-    
+        AtomicInteger sum = new AtomicInteger(0);
+
+        int max = Integer.MIN_VALUE;
+
+        executorService.submit(() -> {
+            for (int number : numbers) {
+                sum.addAndGet(number);
+            }
+        });
+
+        executorService.submit(() -> {
+            for (int number : numbers) {
+                if (number > max) {
+                    max = number;
+                }
+            }
+        });
+
+        executorService.shutdown();
+
+        System.out.println("Tổng: " + sum.get());
+        System.out.println("Phần tử lớn nhất: " + max);
     }
 }
